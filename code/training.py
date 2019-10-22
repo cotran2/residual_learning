22d1e79161e9a1a414f76ae5847b21ba6160beb3
cotran2 <cotran2@utexas.edu>
2019-10-22 18:30:53 -0500

    fixed loss

--- code/training.py
+++ code/training.py
@@ -8,14 +8,14 @@ if tf.__version__ != "2.0.0":
     tf.enable_eager_execution()
 def my_loss(y_pred,y_true):
     y_true = tf.one_hot(y_true, n_outputs, dtype=tf.float32)
-    return  tf.reduce_mean(tf.keras.losses.categorical_crossentropy(y_true,y_pred))
+    return  tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y_true,y_pred))
 """
     Hyper-parameters
 """
 n_inputs = 28*28
 n_outputs = 10
-n_hiddens = 200
-n_epochs = 5
+n_hiddens = 784
+n_epochs = 20
 n_batches = 1000
 epoch = 0
 target_loss = 1e-5
@@ -25,7 +25,7 @@ thresh_hold = 1e-5
 """
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+x_train, x_test = x_train / 1.0, x_test / 1.0
 x_train = x_train.reshape((len(x_train),28*28))
 x_test = x_test.reshape((len(x_test),28*28))
 train_dataset = tf.data.Dataset.from_tensor_slices((x_train,y_train)).shuffle(len(x_train)).batch(n_batches)
@@ -40,8 +40,8 @@ while (epoch < n_epochs) or (epoch_loss_avg < target_loss):
     accuracy = 0
     for x, y in train_dataset:
         with tf.GradientTape() as tape:
-            out_sm = m_model(x)
-            batch_loss = my_loss(out_sm, y)
+            out = m_model(x)
+            batch_loss = my_loss(out, y)
             variables = m_model.trainable_variables
             grads = tape.gradient(batch_loss, variables)
             optimizer.apply_gradients(zip(grads, variables))
